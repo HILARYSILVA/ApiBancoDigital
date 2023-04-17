b815bb9f79150903bd039b100c5a8db05aa9ba98
HILARYSILVA <hilaryr646@gmail.com>
2023-04-17 09:14:49 -0300

    inserts rotas

---
+++ rotas.php
@@ -0,0 +1,23 @@
+<?php
+
+use API\Controller\Controller;
+
+$url = parse_url($_SERVER['REQUEST_UR1'], PHP_URL_PATH);
+
+switch ($url)
+{
+    case'/correntista/save';
+    break;
+
+    case'/conta/extrato';
+    break;
+
+    case'/conta/pix/enviar';
+    break;
+
+    case'/conta/pix/receber';
+    break;
+
+    case'/correntista/entrar';
+    break;
+}
