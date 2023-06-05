62dd7a83155a095cfc40886ed34bf3a798438a20
HILARYSILVA <hilaryr646@gmail.com>
2023-06-05 09:23:29 -0300

    Atualizando DAO

--- DAO/CorrentistaDAO.php
+++ DAO/CorrentistaDAO.php
@@ -2,45 +2,46 @@
 
 namespace ApiBancoDigital\DAO;
 
-use ApiBancoDigital\Model\CorrenteModel;
+use ApiBancoDigital\Model\CorrentistaModel;
+use PDO;
+
 
 class CorrentistaDAO extends DAO
 {
-        
+
     public function __construct()
     {
-      
+       
         parent::__construct();       
     }
 
-   
+ 
     public function save(CorrentistaModel $m) : CorrentistaModel
     {
         return ($m->id == null) ? $this->insert($m) : $this->update($m);
     }
 
 
-  
+ 
     private function insert(CorrentistaModel $model)
     {
-        
+
         $sql = "INSERT INTO correntista
                             (nome, email, cpf, data_nascimento, senha) 
                 VALUES 
                             (?, ?, ?, ?, sha1(?) ) ";
 
-       
+        
         $stmt = $this->conexao->prepare($sql);
 
 
-    
+
         $stmt->bindValue(1, $model->nome);
         $stmt->bindValue(2, $model->email);
         $stmt->bindValue(3, $model->cpf);
         $stmt->bindValue(4, $model->data_nascimento);
         $stmt->bindValue(5, $model->senha);
 
-         
         $stmt->execute();
 
         $model->id = $this->conexao->lastInsertId();
@@ -49,13 +50,11 @@ class CorrentistaDAO extends DAO
     }
 
 
-    
     private function update(CorrentistaModel $m) {
 
     }
 
 
-    
     public function selectByCpfAndSenha($cpf, $senha) : CorrentistaModel
     {
         $sql = "SELECT * FROM correntista WHERE cpf = ? AND senha = sha1(?) ";
@@ -65,8 +64,9 @@ class CorrentistaDAO extends DAO
         $stmt->bindValue(2, $senha);
         $stmt->execute();
 
-        return $stmt->fetchObject("App\Model\CorrentistaModel"); // Retornando um objeto específico PessoaModel
+        return $stmt->fetchObject("ApiBancoDigital\Model\CorrentistaModel"); 
     }
 
 
+   
 }
