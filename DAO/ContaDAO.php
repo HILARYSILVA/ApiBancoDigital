7aa2f64c9ad999da7731493cc489eafb35f9f20b
HILARYSILVA <hilaryr646@gmail.com>
2023-07-27 14:54:47 -0300

    update

--- DAO/ContaDAO.php
+++ DAO/ContaDAO.php
@@ -8,47 +8,37 @@ use \PDO;
 
 class ContaDAO extends DAO
 {
-    
+
     public function __construct()
     {
-        
+      
         parent::__construct();       
     }
 
 
- 
-    public function insert(ContaModel $model)
+    public function insert(ContaModel $model) : ?ContaModel
     {
-
-        $sql = "INSERT INTO Reclamacao 
-                            (id_categoria, id_cidadao, id_bairro, descricao, titulo, endereco, latitude, longitude, foto) 
+    
+        $sql = "INSERT INTO conta 
+                            (id_correntista, saldo, limite, tipo) 
                 VALUES 
-                            (?, ?, ?, ?, ?, ?, ?, ?, ?, ?) ";
+                            (?, ?, ?, ?) ";
 
-
-       
         $stmt = $this->conexao->prepare($sql);
 
+        $stmt->bindValue(1, $model->id_correntista);
+        $stmt->bindValue(2, $model->saldo);
+        $stmt->bindValue(3, $model->limite);
+        $stmt->bindValue(4, $model->tipo);
 
-        
-        $stmt->bindValue(1, $model->id_categoria);
-        $stmt->bindValue(2, $model->id_cidadao);
-        $stmt->bindValue(3, $model->id_bairro);
-        $stmt->bindValue(4, $model->descricao);
-        $stmt->bindValue(5, $model->titulo);
-        $stmt->bindValue(6, $model->endereco);
-        $stmt->bindValue(7, $model->latitude);
-        $stmt->bindValue(8, $model->longitude);
-        $stmt->bindValue(9, $model->foto);
+        $stmt->execute();
 
-         
-        return $stmt->execute();
+        $model->id = $this->conexao->lastInsertId();
+
+        return $model;
     }
 
 
-   
-
-    
     public function select(int $id_cidadao)
     {
         $sql = "SELECT * FROM Reclamacao WHERE id_cidadao = ? ";
@@ -57,10 +47,7 @@ class ContaDAO extends DAO
         $stmt->bindValue(1, $id_cidadao);
         $stmt->execute();
 
-   
         return $stmt->fetchAll(PDO::FETCH_CLASS);        
     }
 
-
-   
 }
--- Model/ContaModel.php
+++ Model/ContaModel.php
@@ -7,37 +7,30 @@ use ApiBancoDigital\DAO\ContaDAO;
 
 class ContaModel extends Model
 {
-    public $id, $id_categoria, $id_cidadao, $id_bairro, $descricao, $titulo, $endereco, $latitude, $longitude, $foto;
 
-
+    public $id, $id_correntista, $saldo, $limite, $tipo, $data_abertura;
 
     public function save()
     {
-       
-        $dao = new ContaDAO(); 
 
+        $dao = new ContaDAO(); 
 
         if(empty($this->id))
         {
-           
+
             $dao->insert($this);
 
         } else {
 
-            
         }        
     }
 
-
     public function getAllRows(int $id_cidadao)
     {      
-
         $dao = new ContaDAO();
 
-    
         $this->rows = $dao->select($id_cidadao);
     }
 
 
-   
 }
